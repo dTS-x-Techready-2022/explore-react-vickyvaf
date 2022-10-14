a05b37f3833306f3b4ec41ba6a378930e85b8ff6
vickyvaf <vickyadi243@gmail.com>
2022-10-14 18:09:21 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Auction from './components/Auction';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Auction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Auction.js
@@ -0,0 +1,36 @@
+import { Button, Typography } from "@mui/material";
+import iauction from "iauction";
+import { useEffect, useState } from "react";
+
+const Auction = () => {
+  const [teks, setTeks] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [timer, setTimer] = useState("");
+
+  useEffect(() => {
+    iauction({
+      countdownInMin: 1,
+      startDate: "2022/10/10 17:30:00",
+      endDate: "2022/10/15 18:23:00",
+      callback: (time) => {
+        // console.log(time.time);
+        let times = time.time;
+        setTimer(time.time);
+        if (times === "00:00") {
+          setTeks(false);
+          setIsOpen(true);
+        }
+      },
+    });
+  }, []);
+  return (
+    <>
+      {teks && (
+        <Typography>Dalam waktu {timer} tombol beli akan muncul 🤑</Typography>
+      )}
+      {isOpen && <Button>BUY NOW</Button>}
+    </>
+  );
+};
+
+export default Auction;
